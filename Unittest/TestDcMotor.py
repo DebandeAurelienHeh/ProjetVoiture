ace248d3fd189171c025713e4f6c76af004bce86
DebandeAurelienHeh <aurelien.debande@std.heh.be>
2025-04-08 11:56:47 +0200

    refactor: creation of setUp

--- Unittest/TestDcMotor.py
+++ Unittest/TestDcMotor.py
@@ -5,26 +5,26 @@ from DCMotor import DCMotor
 
 class TestDCMotor(unittest.TestCase):
 
+    def setUp(self):
+        self.motor = DCMotor(1, 2, 3)
+
     @patch("DCMotor.GPIO")
     def test_set_direction_forward(self, mock_gpio):
-        motor = DCMotor(1, 2, 3)
-        motor.setDirection(True)
+        self.motor.setDirection(True)
 
         mock_gpio.output.assert_any_call(2, mock_gpio.HIGH)
         mock_gpio.output.assert_any_call(3, mock_gpio.LOW)
 
     @patch("DCMotor.GPIO")
     def test_set_direction_backward(self, mock_gpio):
-        motor = DCMotor(1, 2, 3)
-        motor.setDirection(False)
+        self.motor.setDirection(False)
 
         mock_gpio.output.assert_any_call(2, mock_gpio.LOW)
         mock_gpio.output.assert_any_call(3, mock_gpio.HIGH)
 
     @patch("DCMotor.GPIO")
     def test_stop_motor(self, mock_gpio):
-        motor = DCMotor(1, 2, 3)
-        motor.stop()
+        self.motor.stop()
 
         mock_gpio.output.assert_any_call(2, mock_gpio.HIGH)
         mock_gpio.output.assert_any_call(3, mock_gpio.HIGH)
