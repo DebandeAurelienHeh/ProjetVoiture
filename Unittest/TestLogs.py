6eaa389216b19d4218b56e206cb81c9ac0734e3d
DebandeAurelienHeh <aurelien.debande@std.heh.be>
2025-04-10 12:03:54 +0200

    fix: Try except fileNotFoundError add

--- Unittest/TestLogs.py
+++ Unittest/TestLogs.py
@@ -6,10 +6,16 @@ import os
 
 class TestLogs(unittest.TestCase):
     def setUp(self):
+        today_str = str(datetime.datetime.now().date())
+        log_dir = "logs"
+        os.makedirs(log_dir, exist_ok=True)
+
         logging.basicConfig(
-            level=logging.DEBUG,
+            filename=os.path.join(log_dir, f"LamboCar_{today_str}.log"),
+            level=logging.INFO,
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            )
+        )
+
         self.logger = logging.getLogger(__name__)
 
 
@@ -34,15 +40,21 @@ class TestLogs(unittest.TestCase):
         self.assertEqual(cm.output, ['ERROR:test:first message'])
     
 
-    def test_file_content(self):
-        log_path = os.path.join(
-            "logs",
-            f"LamboCar_{datetime.datetime.now().date()}.log"
-        )
-        
-        with open(log_path, "r") as file:
-            content = file.read()
-            self.assertIn(str(datetime.datetime.now().date()), content)
+    def test_in_file_content(self):
+        try:
+            log_path = os.path.join(
+                "logs",
+                f"LamboCar_{datetime.datetime.now().date()}.log"
+            )
+
+            self.logger.info("Add log to check the log file.")
+
+            with open(log_path, "r") as file:
+                content = file.read()
+                self.assertIn("Add log to check the log file.", content)
+
+        except FileNotFoundError:
+            self.fail(f"Log file {log_path} not found.")
     
 
 if __name__ == "__main__":
